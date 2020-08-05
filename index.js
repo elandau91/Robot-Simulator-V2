ea665a80ab0357432a05ffa99bfb1a63dbd08194
elandau91 <eli.b.landau@gmail.com>
2020-08-05 12:05:38 -0400

    group edits

--- index.js
+++ index.js
@@ -3,4 +3,33 @@ document.addEventListener("DOMContentLoaded", function(){
   renderBot(currentPosition)
 
   // ADD CODE HERE!
+  let moves = document.getElementById("moves-container")
+  let button = document.getElementById("move-button")
+
+document.addEventListener("keydown", function(event) {
+  let keyClick = document.createElement("li")
+  moves.append(keyClick)
+
+  let keyOptions = {"ArrowUp": "Up", "ArrowDown": "Down", "ArrowLeft": "Left", "ArrowRight": "Right"}
+
+    keyClick.innerText = keyOptions[event.key]
+
+    if (keyClick.innerText == "undefined") {
+      keyClick.remove()
+    }
+
+});
+
+
+button.addEventListener("click", function(event) {
+  let bullet = document.querySelector("li")
+
+  move(bullet.innerText.toLowerCase())
+
+  bullet.remove()
+  event.preventDefault()
 })
+
+
+})
+
